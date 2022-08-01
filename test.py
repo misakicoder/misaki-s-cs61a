d04a6bd198153d13d1edd03250cff7700d09f240
Misaki、 <misaki@MisakideMacBook-Pro.local>
2022-08-01 15:53:59 +0800

    完成lab07

--- test.py
+++ test.py
@@ -1,31 +1,7 @@
-def permutations(seq):
-    """Generates all permutations of the given sequence. Each permutation is a
-    list of the elements in SEQ in a different order. The permutations may be
-    yielded in any order.
+def test():
+    yield 1
+    yield 2
 
-    >>> perms = permutations([100])
-    >>> type(perms)
-    <class 'generator'>
-    >>> next(perms)
-    [100]
-    >>> try: #this piece of code prints "No more permutations!" if calling next would cause an error
-    ...     next(perms)
-    ... except StopIteration:
-    ...     print('No more permutations!')
-    No more permutations!
-    >>> sorted(permutations([1, 2, 3])) # Returns a sorted list containing elements of the generator
-    [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
-    >>> sorted(permutations((10, 20, 30)))
-    [[10, 20, 30], [10, 30, 20], [20, 10, 30], [20, 30, 10], [30, 10, 20], [30, 20, 10]]
-    >>> sorted(permutations("ab"))
-    [['a', 'b'], ['b', 'a']]
-    """
-    "*** YOUR CODE HERE ***"
-    if type(seq) != list:
-        seq = list(seq)
-    if len(seq) == 1:
-        yield seq
-    else:
-        for elem in permutations(seq[1:]):
-            for i in range(len(seq)):
-                yield elem[:i] + seq[0:1] + elem[i:]
+m = test()
+print(next(m))
+print(next(m))
